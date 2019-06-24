fe59ecb6a18a7e8615bf783a098bb66407924726
Nguyen Duy Kien <35446189+kienduynguyen@users.noreply.github.com>
2019-06-24 22:50:49 +0100

    Update dataset file (Pytorch 1.0 compatible)

--- dense_coattn/data/dataset.py
+++ dense_coattn/data/dataset.py
@@ -1,130 +1,140 @@
 
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
+import collections
+import os
+import random
 
 import h5py
-import random
-import numpy as np
-import os
+import PIL.Image as Image
 import torch
 import torchvision.transforms as transforms
-import PIL.Image as Image
+from torch.utils.data import Dataset
 
 
-type_map = {
-	"train": "",
-	"val": "",
-	"test": "",
-	"testdev": "",
-}
+class VisionLanguageDataset(Dataset):
+
+	def __init__(self, **kwargs):
+		self.__dict__.update(kwargs)
+		data_path = kwargs["data_path"]
+		data_name = kwargs["data_name"]
+		data_type = kwargs["data_type"]
+		data_info_path = os.path.join(data_path, "{}_info.pt".format(data_name))
+		data_file_path = os.path.join(data_path, "{}_{}.h5".format(data_name, data_type))
+		self.data_info = torch.load(data_info_path)
+		self.dataset = h5py.File(data_file_path, "r")
+
+		image_path = kwargs["image_path"]
+		image_type = kwargs["image_type"]
+		image_split = kwargs["image_split"]
+		if image_type == "rcnn":
+			self.imageset = RCNNDataset(image_path, image_split)
+		elif image_type == "resnet":
+			self.imageset = ResnetDataset(image_path, image_split)
+		else:
+			raise TypeError("image dataset type should be 'rcnn, resnet, or fused_resnet' "
+							"detected {}".format(image_type))
 
 
-class Dataset(object):
+class RCNNDataset(Dataset):
 
-	def __init__(self, data_path, data_name, data_type, seq_per_img, img_name, size_scale=None, use_h5py=True):
-		"""
-		Load all of dataset information when initialize the Dataset object. 
-		--------------------
-		Parameters:
-			data_path (str): path points to the folder containing data file.
-			data_name (str): name of the dataset.
-			data_type (str): "train", "val", "trainval", "test", or "testdev".
-			seq_per_img (int): number of questions loaded with the same image.
-			img_name (str): name of data file storing the image data.
-			size_scale (int or tuple): If use_h5py is False, this is used to scale images.
-			use_h5py (bool): If True, load image data from h5py instead of disk.
-		"""
-		img_type = data_type if data_type in ["train", "val", "trainval"] else "test"
-		data_info = os.path.join(data_path, "%s_info.pt" % (data_name))
-		data_file = os.path.join(data_path, "%s_%s.h5" % (data_name, data_type))
-		img_file = os.path.join(data_path, "%s_%s.h5" % (img_name, img_type)) if use_h5py else None
-		img_info = os.path.join(data_path, "%s_%s.pt" % (img_name, img_type)) if use_h5py else None
+	def __init__(self, data_path, data_type):
+		super(RCNNDataset, self).__init__()
+		img_info_path = os.path.join(data_path, "rcnn_{}.pt".format(data_type))
+		self.img2idx = torch.load(img_info_path)
 
-		self.data_type = data_type
-		self.seq_per_img = seq_per_img
-		self.data_info = torch.load(data_info)
+		self.img_file_path = os.path.join(data_path, "rcnn_{}.h5".format(data_type))
+		self.imageset = None
+		self.features = None
+		self.img_start_idx = None
+		self.img_end_idx = None
 
-		self.img2idx = torch.load(img_info) if use_h5py else None
+	def _initialize(self):
+		if self.imageset is None or self.features is None or self.img_start_idx is None or self.img_end_idx is None:
+			self.imageset = h5py.File(self.img_file_path, "r")
+			self.features = self.imageset["features"]
+			self.img_start_idx = self.imageset["img_start_idx"]
+			self.img_end_idx = self.imageset["img_end_idx"]
+
+	def __getitem__(self, index):
+		self._initialize()
+		img_idx = self.img2idx[index]
+		img_start_idx = self.img_start_idx[img_idx]
+		img_end_idx = self.img_end_idx[img_idx]
+		nboxes = img_end_idx - img_start_idx + 1
+
+		img = torch.zeros(1, 100, 2048, dtype=torch.float32)
+		# img[0, :nboxes, :].copy_(torch.from_numpy(self.features[img_start_idx:img_end_idx+1,:]),
+		#   non_blocking=True)
+		img[0, :nboxes, :] = torch.from_numpy(self.features[img_start_idx:img_end_idx+1,:])
+		img_mask = torch.zeros(1, 100, dtype=torch.float32)
+		img_mask[0, :nboxes] = 1
+
+		return (img, img_mask)
+
+	def __len__(self):
+		return len(self.img2idx)
+
+
+class ResnetDataset(Dataset):
+
+	def __init__(self, data_path, data_type):
+		super(ResnetDataset, self).__init__()
+		img_info_path = os.path.join(data_path, "resnet_{}.pt".format(data_type))
+		self.img2idx = torch.load(img_info_path)
+		self.transform = transforms.Compose([
+				transforms.ToTensor(),
+				transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+			])
+
+		self.img_file_path = os.path.join(data_path, "resnet_{}.h5".format(data_type))
+		self.imageset = None
+		self.images = None
+	
+	def _initialize(self):
+		if self.imageset is None or self.images is None:
+			self.imageset = h5py.File(self.img_file_path, "r")
+			self.images = self.imageset["images"]
+
+	def __getitem__(self, index):
+		self._initialize()
+		img_idx = self.img2idx[index]
+		img = self.transform(Image.fromarray(self.images[img_idx])).unsqueeze(0)
+
+		return img
+
+	def __len__(self):
+		return len(self.img2idx)
+
+
+class VQADataset(VisionLanguageDataset):
+
+	def __init__(self, data_path, data_name, data_type, image_path, image_type, image_split):
+		super(VQADataset, self).__init__(data_path=data_path, data_name=data_name, 
+			data_type=data_type, image_path=image_path, image_type=image_type, image_split=image_split)
+
 		self.idx2word = self.data_info["idx2word"]
 		self.idx2ans = self.data_info["idx2ans"]
 		self.word2idx = self.data_info["word2idx"]
-		self.ans2idx = self.data_info["ans2idx"]
+		self.and2idx = self.data_info["ans2idx"]
 
-		self.dataset = h5py.File(data_file, "r", driver="core")
-		self.imageset = h5py.File(img_file, "r") if use_h5py else None
-
-		self.images = self.imageset["images"][:] if use_h5py else None
 		self.img_idx = self.dataset["img_idx"][:]
-		self.txt_start_idx = self.dataset["txt_start_idx"][:]
-		self.txt_end_idx = self.dataset["txt_end_idx"][:]
 		self.questions = self.dataset["questions"][:]
 		self.ques_idx = self.dataset["ques_idx"][:]
 		self.ans_pool = self.dataset["ans_pool"][:]
-		self.ans_idx = self.dataset["ans_idx"][:] if not data_type in ["test", "testdev"] else None
-
-		if use_h5py:
-			self.transform = transforms.Compose([
-					transforms.ToTensor(),
-					transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-				])
+		
+		if "test" in data_type:
+			self.ans_idx = None
 		else:
-			self.transform = transforms.Compose([
-				transforms.Resize(size_scale),
-				transforms.ToTensor(),
-				transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-			])
+			self.ans_idx = self.dataset["ans_idx"][:]
+		print(f"Initializing {data_type} VQA dataset: {self.questions.shape[0]} questions, {len(self.imageset)} images")
 
-		print("Vocabulary size:", len(self.idx2word))
-		print("Number of question:", self.img_idx.shape[0])
-		print("Dataset %s_%s loaded" % (data_name, data_type))
+	def __getitem__(self, index):
+		ques = torch.from_numpy(self.questions[[index]])
+		ques_idx = torch.from_numpy(self.ques_idx[index])
+		ans_idx = torch.from_numpy(self.ans_idx[[index]]) if self.ans_idx is not None else None
+		ques_mask = ques.ne(0).float()
+		img_info = self.imageset[self.img_idx[index]]
 
-	def __getitem__(self, idx):
-		"""
-		Arguments:
-			idx (int): index of tuple (img, ques, ans) needed to extract from the dataset.
-		Return:
-			samples (tuple): contains image, question, question mask, index and answer score data.
-		"""
-		idx_start = self.txt_start_idx[idx]
-		idx_end = self.txt_end_idx[idx]
-		ntxt = idx_end - idx_start + 1
-		assert ntxt > 0, "An image doesn't have any questions & answers!"
-
-		img_idx = self.img_idx[idx]
-
-		if self.img2idx is not None:
-			img = self.transform(Image.fromarray(self.images[self.img2idx[img_idx]])).unsqueeze(0)
-		else:
-			if self.data_type != "trainval":
-				img = self.transform(Image.open("%s_%.12i.jpg" % (type_map[self.data_type], img_idx))\
-										.convert("RGB")).unsqueeze(0)
-			else:
-				if os.path.isfile("%s_%.12i.jpg" % (type_map["train"], img_idx)):
-					img = self.transform(Image.open("%s_%.12i.jpg" % (type_map["train"], img_idx))\
-											.convert("RGB")).unsqueeze(0)
-				else:
-					img = self.transform(Image.open("%s_%.12i.jpg" % (type_map["val"], img_idx))\
-											.convert("RGB")).unsqueeze(0)
-
-		if ntxt < self.seq_per_img:
-			indices = [random.randint(idx_start, idx_end) for _ in range(ntxt)]
-			ques = self.questions[indices]
-			ques_idx = self.ques_idx[indices]
-			ans_idx = self.ans_idx[indices] if self.ans_idx is not None else None
-		else:
-			index = random.randint(idx_start, idx_end - self.seq_per_img + 1)
-			ques = self.questions[index:index+self.seq_per_img]
-			ques_idx = self.ques_idx[index:index+self.seq_per_img]
-			ans_idx = self.ans_idx[index:index+self.seq_per_img] if self.ans_idx is not None else None
-		ques_mask = np.not_equal(ques, 0).astype(np.float32)
-
-		if self.ans_idx is None:
-			sample = (img, ques, ques_mask, ques_idx)
-		else:
-			sample = (img, ques, ques_mask, ques_idx, ans_idx)
-
-		return sample
+		return (img_info, ques, ques_mask, ans_idx, ques_idx)
 
 	def __len__(self):
 		return self.questions.shape[0]
