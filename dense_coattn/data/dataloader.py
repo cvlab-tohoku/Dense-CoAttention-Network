c26ddc974ddd53c53fc106a3961135c2f47fce92
Nguyen Duy Kien <35446189+kienduynguyen@users.noreply.github.com>
2019-06-24 22:51:28 +0100

    Update dataloader (Pytorch 1.0 compatible)

--- dense_coattn/data/dataloader.py
+++ dense_coattn/data/dataloader.py
@@ -1,22 +1,21 @@
 
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
+import collections
+import sys
+import threading
+import traceback
 
 import torch
 import torch.multiprocessing as multiprocessing
-import collections
-import sys
-import traceback
-import threading
-if sys.version_info[0] == 2:
-	import Queue as queue
-	string_classes = basestring
-else:
-	import queue
-	string_classes = (str, bytes)
 
-from .sampler import RandomSampler, SequentialSampler, BatchSampler
+from .sampler import BatchSampler, RandomSampler, SequentialSampler
+
+if sys.version_info[0] == 2:
+    import Queue as queue
+    string_classes = basestring
+else:
+    import queue
+    string_classes = (str, bytes)
+
 
 
 _use_shared_memory = False
@@ -24,271 +23,271 @@ _use_shared_memory = False
 
 class ExceptionWrapper(object):
 
-	def __init__(self, exc_info):
-		self.exc_type = exc_info[0]
-		self.exc_msg = "".join(traceback.format_exception(*exc_info))
+    def __init__(self, exc_info):
+        self.exc_type = exc_info[0]
+        self.exc_msg = "".join(traceback.format_exception(*exc_info))
 
 
 def _worker_thread_loop(dataset, index_queue, data_queue, collate_fn):
-	while True:
-		r = index_queue.get()
-		if r is None:
-			data_queue.put(None)
-			break
-		idx, batch_indices = r
-		try:
-			samples = collate_fn([dataset[i] for i in batch_indices])
-		except Exception:
-			data_queue.put((idx, ExceptionWrapper(sys.exc_info())))
-		else:
-			data_queue.put((idx, samples))
+    while True:
+        r = index_queue.get()
+        if r is None:
+            data_queue.put(None)
+            break
+        idx, batch_indices = r
+        try:
+            samples = collate_fn([dataset[i] for i in batch_indices])
+        except Exception:
+            data_queue.put((idx, ExceptionWrapper(sys.exc_info())))
+        else:
+            data_queue.put((idx, samples))
 
 
 def _worker_process_loop(dataset, index_queue, data_queue, collate_fn):
-	global _use_shared_memory
-	_use_shared_memory = True
+    global _use_shared_memory
+    _use_shared_memory = True
 
-	torch.set_num_threads(1)
-	while True:
-		r = index_queue.get()
-		if r is None:
-			data_queue.put(None)
-			break
-		idx, batch_indices = r
-		try:
-			samples = collate_fn([dataset[i] for i in batch_indices])
-		except Exception:
-			data_queue.put((idx, ExceptionWrapper(sys.exc_info())))
-		else:
-			data_queue.put((idx, samples))
+    torch.set_num_threads(1)
+    while True:
+        r = index_queue.get()
+        if r is None:
+            data_queue.put(None)
+            break
+        idx, batch_indices = r
+        try:
+            samples = collate_fn([dataset[i] for i in batch_indices])
+        except Exception:
+            data_queue.put((idx, ExceptionWrapper(sys.exc_info())))
+        else:
+            data_queue.put((idx, samples))
 
 
 def _pin_memory_loop(in_queue, out_queue, done_event):
-	while True:
-		try:
-			r = in_queue.get()
-		except:
-			if done_event.is_set():
-				out_queue.put(None)
-				return
-			raise
-		if r is None:
-			break
-		if isinstance(r[1], ExceptionWrapper):
-			out_queue.put(r)
-			continue
-		idx, batch = r
-		try:
-			batch = pin_memory_batch(batch)
-		except Exception:
-			out_queue.put((idx, ExceptionWrapper(sys.exc_info())))
-		else:
-			out_queue.put((idx, batch))
+    while True:
+        try:
+            r = in_queue.get()
+        except:
+            if done_event.is_set():
+                out_queue.put(None)
+                return
+            raise
+        if r is None:
+            break
+        if isinstance(r[1], ExceptionWrapper):
+            out_queue.put(r)
+            continue
+        idx, batch = r
+        try:
+            batch = pin_memory_batch(batch)
+        except Exception:
+            out_queue.put((idx, ExceptionWrapper(sys.exc_info())))
+        else:
+            out_queue.put((idx, batch))
 
 
 numpy_type_map = {
-	"float64": torch.DoubleTensor,
-	"float32": torch.FloatTensor,
-	"float16": torch.HalfTensor,
-	"int64": torch.LongTensor,
-	"int32": torch.IntTensor,
-	"int16": torch.ShortTensor,
-	"int8": torch.CharTensor,
-	"uint8": torch.ByteTensor,
+    "float64": torch.DoubleTensor,
+    "float32": torch.FloatTensor,
+    "float16": torch.HalfTensor,
+    "int64": torch.LongTensor,
+    "int32": torch.IntTensor,
+    "int16": torch.ShortTensor,
+    "int8": torch.CharTensor,
+    "uint8": torch.ByteTensor,
 }
 
 
 def default_collate(batch):
-	if torch.is_tensor(batch[0]):
-		out = None
-		if _use_shared_memory:
-			numel = sum([x.numel() for x in batch])
-			storage = batch[0].storage()._new_shared(numel)
-			out = batch[0].new(storage)
-		return torch.cat(batch, 0, out=out)
-	elif type(batch[0]).__module__ == "numpy":
-		elem = batch[0]
-		if type(elem).__name__ == "ndarray":
-			return torch.cat([torch.from_numpy(b) for b in batch], 0)
-		if elem.shape == ():
-			py_type = float if elem.dtype.name.startswith("float") else int
-			return numpy_type_map[elem.dtype.name](list(map(py_type, batch)))
-	elif isinstance(batch[0], int):
-		return torch.LongTensor(batch)
-	elif isinstance(batch[0], float):
-		return torch.DoubleTensor(batch)
-	elif isinstance(batch[0], string_classes):
-		return batch
-	elif isinstance(batch[0], collections.Mapping):
-		return {key: default_collate([d[key] for d in batch]) for key in batch[0]}
-	elif isinstance(batch[0], collections.Sequence):
-		transposed = zip(*batch)
-		return [default_collate(samples) for samples in transposed]
+    if torch.is_tensor(batch[0]):
+        out = None
+        if _use_shared_memory:
+            numel = sum([x.numel() for x in batch])
+            storage = batch[0].storage()._new_shared(numel)
+            out = batch[0].new(storage)
+        return torch.cat(batch, 0, out=out)
+    elif type(batch[0]).__module__ == "numpy":
+        elem = batch[0]
+        if type(elem).__name__ == "ndarray":
+            return torch.cat([torch.from_numpy(b) for b in batch], 0)
+        if elem.shape == ():
+            py_type = float if elem.dtype.name.startswith("float") else int
+            return numpy_type_map[elem.dtype.name](list(map(py_type, batch)))
+    elif isinstance(batch[0], int):
+        return torch.LongTensor(batch)
+    elif isinstance(batch[0], float):
+        return torch.DoubleTensor(batch)
+    elif isinstance(batch[0], string_classes):
+        return batch
+    elif isinstance(batch[0], collections.Mapping):
+        return {key: default_collate([d[key] for d in batch]) for key in batch[0]}
+    elif isinstance(batch[0], collections.Sequence):
+        transposed = zip(*batch)
+        return [default_collate(samples) for samples in transposed]
 
-	raise TypeError(("batch muse contains tensors, numbers, dicts or lists; found {}".format(type(batch[0]))))
+    raise TypeError(("batch muse contains tensors, numbers, dicts or lists; found {}".format(type(batch[0]))))
 
 
 def pin_memory_batch(batch):
-	if torch.is_tensor(batch):
-		return batch.pin_memory()
-	elif isinstance(batch, string_classes):
-		return batch
-	elif isinstance(batch, collections.Mapping):
-		return {k: pin_memory_batch(sample) for k, sample in batch.items()}
-	elif isinstance(batch, collections.Sequence):
-		return [pin_memory_batch(sample) for sample in batch]
-	else:
-		return batch
+    if torch.is_tensor(batch):
+        return batch.pin_memory()
+    elif isinstance(batch, string_classes):
+        return batch
+    elif isinstance(batch, collections.Mapping):
+        return {k: pin_memory_batch(sample) for k, sample in batch.items()}
+    elif isinstance(batch, collections.Sequence):
+        return [pin_memory_batch(sample) for sample in batch]
+    else:
+        return batch
 
 
 class DataLoaderIter(object):
 
-	def __init__(self, loader):
-		self.dataset = loader.dataset
-		self.collate_fn = loader.collate_fn
-		self.batch_sampler = loader.batch_sampler
-		self.num_workers = loader.num_workers
-		self.pin_memory = loader.pin_memory
-		self.use_thread = loader.use_thread
-		self.pin_done_event = threading.Event()
+    def __init__(self, loader):
+        self.dataset = loader.dataset
+        self.collate_fn = loader.collate_fn
+        self.batch_sampler = loader.batch_sampler
+        self.num_workers = loader.num_workers
+        self.pin_memory = loader.pin_memory
+        self.use_thread = loader.use_thread
+        self.pin_done_event = threading.Event()
 
-		self.sample_iter = iter(self.batch_sampler)
+        self.sample_iter = iter(self.batch_sampler)
 
-		if self.num_workers > 0:
-			self.index_queue = queue.Queue() if self.use_thread else multiprocessing.SimpleQueue()
-			self.data_queue = queue.Queue() if self.use_thread else multiprocessing.SimpleQueue()
-			self.batches_outstanding = 0
-			self.shutdown = False
-			self.send_idx = 0
-			self.rcvd_idx = 0
-			self.reorder_dict = {}
+        if self.num_workers > 0:
+            self.index_queue = queue.Queue() if self.use_thread else multiprocessing.SimpleQueue()
+            self.data_queue = queue.Queue() if self.use_thread else multiprocessing.SimpleQueue()
+            self.batches_outstanding = 0
+            self.shutdown = False
+            self.send_idx = 0
+            self.rcvd_idx = 0
+            self.reorder_dict = {}
 
-			if self.use_thread:
-				self.workers = [
-					threading.Thread(
-						target=_worker_thread_loop,
-						args=(self.dataset, self.index_queue, self.data_queue, self.collate_fn))
-					for _ in range(self.num_workers)]
-			else:
-				self.workers = [
-					multiprocessing.Process(
-						target=_worker_process_loop,
-						args=(self.dataset, self.index_queue, self.data_queue, self.collate_fn))
-					for _ in range(self.num_workers)]
+            if self.use_thread:
+                self.workers = [
+                    threading.Thread(
+                        target=_worker_thread_loop,
+                        args=(self.dataset, self.index_queue, self.data_queue, self.collate_fn))
+                    for _ in range(self.num_workers)]
+            else:
+                self.workers = [
+                    multiprocessing.Process(
+                        target=_worker_process_loop,
+                        args=(self.dataset, self.index_queue, self.data_queue, self.collate_fn))
+                    for _ in range(self.num_workers)]
 
-			for w in self.workers:
-				w.daemon = True
-				w.start()
+            for w in self.workers:
+                w.daemon = True
+                w.start()
 
-			if self.pin_memory:
-				in_data = self.data_queue
-				self.data_queue = queue.Queue()
-				self.pin_thread = threading.Thread(
-					target=_pin_memory_loop,
-					args=(in_data, self.data_queue, self.pin_done_event))
-				self.pin_thread.daemon = True
-				self.pin_thread.start()
+            if self.pin_memory:
+                in_data = self.data_queue
+                self.data_queue = queue.Queue()
+                self.pin_thread = threading.Thread(
+                    target=_pin_memory_loop,
+                    args=(in_data, self.data_queue, self.pin_done_event))
+                self.pin_thread.daemon = True
+                self.pin_thread.start()
 
-			for _ in range(2 * self.num_workers):
-				self._put_indices()
+            for _ in range(2 * self.num_workers):
+                self._put_indices()
 
-	def __len__(self):
-		return len(self.batch_sampler)
+    def __len__(self):
+        return len(self.batch_sampler)
 
-	def __next__(self):
-		if self.num_workers == 0:
-			indices = next(self.sample_iter)
-			batch = self.collate_fn([self.dataset[i] for i in indices])
-			if self.pin_memory:
-				batch = pin_memory_batch(batch)
-			return batch
+    def __next__(self):
+        if self.num_workers == 0:
+            indices = next(self.sample_iter)
+            batch = self.collate_fn([self.dataset[i] for i in indices])
+            if self.pin_memory:
+                batch = pin_memory_batch(batch)
+            return batch
 
-		if self.rcvd_idx in self.reorder_dict:
-			batch = self.reorder_dict.pop(self.rcvd_idx)
-			return self._process_next_batch(batch)
+        if self.rcvd_idx in self.reorder_dict:
+            batch = self.reorder_dict.pop(self.rcvd_idx)
+            return self._process_next_batch(batch)
 
-		if self.batches_outstanding == 0:
-			self._shutdown_workers()
-			raise StopIteration
+        if self.batches_outstanding == 0:
+            self._shutdown_workers()
+            raise StopIteration
 
-		while True:
-			assert (not self.shutdown and self.batches_outstanding > 0)
-			idx, batch = self.data_queue.get()
-			self.batches_outstanding -= 1
-			if idx != self.rcvd_idx:
-				self.reorder_dict[idx] = batch
-				continue
-			return self._process_next_batch(batch)
+        while True:
+            assert (not self.shutdown and self.batches_outstanding > 0)
+            idx, batch = self.data_queue.get()
+            self.batches_outstanding -= 1
+            if idx != self.rcvd_idx:
+                self.reorder_dict[idx] = batch
+                continue
+            return self._process_next_batch(batch)
 
-	next = __next__
+    next = __next__
 
-	def __iter__(self):
-		return self
+    def __iter__(self):
+        return self
 
-	def _put_indices(self):
-		assert self.batches_outstanding < 2 * self.num_workers
-		indices = next(self.sample_iter, None)
-		if indices is None:
-			return
-		self.index_queue.put((self.send_idx, indices))
-		self.batches_outstanding += 1
-		self.send_idx += 1
+    def _put_indices(self):
+        assert self.batches_outstanding < 2 * self.num_workers
+        indices = next(self.sample_iter, None)
+        if indices is None:
+            return
+        self.index_queue.put((self.send_idx, indices))
+        self.batches_outstanding += 1
+        self.send_idx += 1
 
-	def _process_next_batch(self, batch):
-		self.rcvd_idx += 1
-		self._put_indices()
-		if isinstance(batch, ExceptionWrapper):
-			raise batch.exc_type(batch.exc_msg)
-		return batch
+    def _process_next_batch(self, batch):
+        self.rcvd_idx += 1
+        self._put_indices()
+        if isinstance(batch, ExceptionWrapper):
+            raise batch.exc_type(batch.exc_msg)
+        return batch
 
-	def __getstate__(self):
-		raise NotImplementedError
+    def __getstate__(self):
+        raise NotImplementedError
 
-	def _shutdown_workers(self):
-		if not self.shutdown:
-			self.shutdown = True
-			self.pin_done_event.set()
-			for _ in self.workers:
-				self.index_queue.put(None)
+    def _shutdown_workers(self):
+        if not self.shutdown:
+            self.shutdown = True
+            self.pin_done_event.set()
+            for _ in self.workers:
+                self.index_queue.put(None)
 
-	def __del__(self):
-		if self.num_workers > 0:
-			self._shutdown_workers()
+    def __del__(self):
+        if self.num_workers > 0:
+            self._shutdown_workers()
 
 
 class DataLoader(object):
 
-	def __init__(self, dataset, batch_size, shuffle=False, sampler=None, batch_sampler=None,
-				 num_workers=0, collate_fn=default_collate, pin_memory=False, drop_last=False, use_thread=False):
-		self.dataset = dataset
-		self.batch_size = batch_size
-		self.num_workers = num_workers
-		self.collate_fn = collate_fn
-		self.pin_memory = pin_memory
-		self.drop_last = drop_last
-		self.use_thread = use_thread
+    def __init__(self, dataset, batch_size, shuffle=False, sampler=None, batch_sampler=None,
+                 num_workers=0, collate_fn=default_collate, pin_memory=False, drop_last=False, use_thread=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.collate_fn = collate_fn
+        self.pin_memory = pin_memory
+        self.drop_last = drop_last
+        self.use_thread = use_thread
 
-		if batch_sampler is not None:
-			if batch_size > 1 or shuffle or sampler is not None or drop_last:
-				raise ValueError("batch_sampler is mutually exclusive with batch_size, \
-					shuffle, sampler, and drop_last")
+        if batch_sampler is not None:
+            if batch_size > 1 or shuffle or sampler is not None or drop_last:
+                raise ValueError("batch_sampler is mutually exclusive with batch_size, \
+                    shuffle, sampler, and drop_last")
 
-		if sampler is not None and shuffle:
-			raise ValueError("sampler is mutually exclusive with shuffle")
+        if sampler is not None and shuffle:
+            raise ValueError("sampler is mutually exclusive with shuffle")
 
-		if batch_sampler is None:
-			if sampler is None:
-				if shuffle:
-					sampler = RandomSampler(dataset)
-				else:
-					sampler = SequentialSampler(dataset)
-			batch_sampler = BatchSampler(sampler, batch_size, drop_last)
+        if batch_sampler is None:
+            if sampler is None:
+                if shuffle:
+                    sampler = RandomSampler(dataset)
+                else:
+                    sampler = SequentialSampler(dataset)
+            batch_sampler = BatchSampler(sampler, batch_size, drop_last)
 
-		self.sampler = sampler
-		self.batch_sampler = batch_sampler
+        self.sampler = sampler
+        self.batch_sampler = batch_sampler
 
-	def __iter__(self):
-		return DataLoaderIter(self)
+    def __iter__(self):
+        return DataLoaderIter(self)
 
-	def __len__(self):
-		return len(self.batch_sampler)
+    def __len__(self):
+        return len(self.batch_sampler)
